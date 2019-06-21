f20e880f502de47860798ed09e94456507f71adf
xinyuezhang93 <zhang.xinyue@husky.neu.edu>
2019-06-21 14:54:37 -0700

    save to bigtable

--- service/main.go
+++ service/main.go
@@ -11,6 +11,7 @@ import (
 	"strconv"
 	"strings"
 
+	"cloud.google.com/go/bigtable"
 	"cloud.google.com/go/storage"
 	jwtmiddleware "github.com/auth0/go-jwt-middleware"
 	"github.com/dgrijalva/jwt-go"
@@ -153,7 +154,33 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
 	saveToES(p, id.String())
 
 	// Save to BigTable.
-	//saveToBigTable(p, id)
+	saveToBigTable(p, id.String())
+}
+
+func saveToBigTable(p *Post, id string) {
+	ctx := context.Background()
+	// you must update project name here
+	bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	tbl := bt_client.Open("post")
+	mut := bigtable.NewMutation()
+	t := bigtable.Now()
+
+	mut.Set("post", "user", t, []byte(p.User))
+	mut.Set("post", "message", t, []byte(p.Message))
+	mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+	mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+	err = tbl.Apply(ctx, id, mut)
+	if err != nil {
+		panic(err)
+		return
+	}
+	fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
 }
 
 // Save a post to ElasticSearch
@@ -164,7 +191,6 @@ func saveToES(p *Post, id string) {
 		panic(err)
 		return
 	}
-
 	// Save it to index
 	_, err = es_client.Index().
 		Index(INDEX).
@@ -177,7 +203,6 @@ func saveToES(p *Post, id string) {
 		panic(err)
 		return
 	}
-
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
 }
 
